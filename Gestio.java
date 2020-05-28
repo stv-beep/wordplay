1a646ab97dd1cde071b7c95a82b87b9809830837
Aleix Algueró <59015816+stv-beep@users.noreply.github.com>
2020-05-28 12:34:37 +0200

    Actualització 0.5
    Comentaris afegits

--- Gestio.java
+++ Gestio.java
@@ -8,9 +8,14 @@ public class Gestio {
 	public static Scanner sc = new Scanner(System.in);
 	
 	
+	/**
+	 * Metode que crea el fitxer. Si que al main també el creem, però allí a més, comprovem si ja existeix,
+	 * per a evitar sobreescriure'l. Aquí el creem de nou, borrant les paraules que havien anteriorment.
+	 */
 	public static void creacioFitxer() {
 		try {
             File pp = new File("pp.txt");
+            System.out.println("Creant fitxer...Per favor, ves a l'opció \"AFEGIR\" per a afegir noves parelles");
             
             FileWriter fw = new FileWriter(pp);
             BufferedWriter bw = new BufferedWriter(fw);
@@ -22,16 +27,17 @@ public class Gestio {
 	
 	
 	/**
-	 * Metode per a escriure parells de paraules guardar-les a l'arxiu pp.txt
+	 * Metode per a escriure parells de paraules i guardar-les a l'arxiu pp.txt
 	 */
 	public static void afegir() {
 		System.out.println("Escriu la paraula en català: ");
 		String catala = sc.nextLine();
 		System.out.println("Escriu la paraula en anglès: ");
 		String angles = sc.nextLine();
-		String parella = catala + "-" + angles + "-"; // - per a separar les traduccions, "," per a separar les parelles
+		String parella = catala + "-" + angles + "-"; //"-" per a separar les traduccions, "-" per a separar les parelles
 		
 		try {
+			/*Amb el printstream escrivim al fitxer*/
 			PrintStream escriptor = new PrintStream(new FileOutputStream("pp.txt", true)); 
 			//Append - boolean si es true, llavors les dades s'escriurarn al final del fitxer i no substuiran altres paraules
 			//https://es.stackoverflow.com/questions/68526/agregar-contenido-a-un-archivo-sin-sobrescribir-el-contenido
@@ -43,13 +49,5 @@ public class Gestio {
 		}
 			
 	}
-	
-	
-	/*hola-pola-mola
-	string.split("-")*/
-	
-	
-
-	
 
 }
